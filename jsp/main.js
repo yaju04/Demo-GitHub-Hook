ca872ef8394e4192445dcdb3f84b701842ed302d
etouch <etouch@Macbookpro-18s.local>
2015-10-07 15:29:27 +0530

    commit operation

---
+++ jsp/main.js
@@ -0,0 +1,28 @@
+    var githubhook = require('githubhook');
+    // configure listener for github changes
+    var github = githubhook({/* options */
+		   host: "localhost",
+       port: 8080,
+       path: "/pushchanges",
+       secret: "123456"  
+	  });
+
+    // listen to push on github on branch master
+    /*github.on('push', function (repo, ref, data) {
+    console.log("Listening Push from GitHub");
+		  var branchName = _s.strRightBack(ref, "/");
+		  var fullNameRepository = data.repository.full_name;
+		  console.log("fullNameRepository"+fullNameRepository);
+		  var removedFilesArray = data["head_commit"]["removed"];
+		  var addedFilesArray = data["head_commit"]["added"];
+		  var modifiedFilesArray = data["head_commit"]["modified"];
+    });*/
+    
+    github.on('push', function (event) {
+  console.log('Received a push event for %s to %s',
+    event.payload.repository.name,
+    event.payload.ref)
+});
+
+    // listen to github push
+    github.listen();
